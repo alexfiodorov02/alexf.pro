aae1cf1788d77850026d3771ac09d6efe6e665a1
Alexander Fiodorov-Miller <alexander.fiodorov-miller@autozone.com>
2025-06-30 14:53:57 -0500

    Added GitHub pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Enables static HTML export
+  images: {
+    unoptimized: true,  // For static export
+  },
 };
 
 export default nextConfig;
